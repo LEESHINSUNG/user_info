c7957235d8ccfb23c74fbb3aa6aff365bb5bfc4d
LEESHINSUNG <89188768+LEESHINSUNG@users.noreply.github.com>
2022-05-31 17:49:36 +0900

    user_info_2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // User main info
 const userPicture = document.querySelector(".userPicture");
 const userName = document.querySelector(".userName");
-const userId = document.querySelector(".userId");
+const userGender = document.querySelector(".userGender");
 
 // Info hover
 const hoverNation = document.querySelector(".nation");
@@ -10,29 +10,35 @@ const hoverPhoneNum = document.querySelector(".phoneNum");
 const hoverLocation = document.querySelector(".location");
 const hoverEmail = document.querySelector(".email");
 const displayData = document.querySelector(".displayData");
+const explanation = document.querySelector(".explanation");
 
 fetch("https://randomuser.me/api/") //
   .then((response) => response.json())
   .then((json) => {
     const jsonData = json.results[0];
-    console.log(jsonData);
+    console.log(typeof jsonData["dob"].date);
 
     userPicture.innerHTML = `<img src="${jsonData["picture"].large}" alt="" />`;
     userName.innerHTML = `${jsonData["name"].last} ${jsonData["name"].first}`;
-    userId.innerHTML = jsonData["id"].name;
+    userGender.innerHTML = jsonData["gender"];
+    explanation.innerHTML = "My country is";
     displayData.innerHTML = jsonData["location"].country;
 
-    // addEventListener
+    // addEventListener (mouseover)
     hoverNation.addEventListener("mouseover", () => {
+      explanation.innerHTML = "My country is";
       displayData.innerHTML = jsonData["location"].country;
     });
     hoverBirthday.addEventListener("mouseover", () => {
-      displayData.innerHTML = jsonData["dob"].date;
+      explanation.innerHTML = "My birthday is";
+      displayData.innerHTML = jsonData["dob"].date.substring(0, 10);
     });
     hoverPhoneNum.addEventListener("mouseover", () => {
+      explanation.innerHTML = "My phone number is";
       displayData.innerHTML = jsonData["phone"];
     });
     hoverLocation.addEventListener("mouseover", () => {
+      explanation.innerHTML = "My address is";
       displayData.innerHTML = `${jsonData["location"].street.number} 
       ${jsonData["location"].street.name} 
       ${jsonData["location"].city} 
@@ -40,17 +46,8 @@ fetch("https://randomuser.me/api/") //
       ${jsonData["location"].country}`;
     });
     hoverEmail.addEventListener("mouseover", () => {
+      explanation.innerHTML = "My email is";
       displayData.innerHTML = jsonData["email"];
     });
   })
   .catch(() => console.log("error"));
-
-function infoNation(jsonData) {}
-
-function infoBirthday(jsonData) {}
-
-function infoPhoneNum(jsonData) {}
-
-function infoLocation(jsonData) {}
-
-function infoEmail(jsonData) {}
